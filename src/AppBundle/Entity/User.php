0925f78ed482763a4ca046ba24afb8a1bcc8daa9
Your Name <you@example.com>
2017-12-04 20:57:53 +0200

    Added vendors

---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 04.12.2017
+ * Time: 20:50
+ */
+
+namespace AppBundle\Entity;
+
+
+class User
+{
+    private $id;
+
+    private $username;
+}
